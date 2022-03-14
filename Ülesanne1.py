95563c667f5f76b03f072845a72d651b093eaf40
laurilaanesoo <98745890+laurilaanesoo@users.noreply.github.com>
2022-03-14 10:51:50 +0200

    Update Ülesanne1.py

--- Ülesanne1.py
+++ Ülesanne1.py
@@ -4,64 +4,23 @@
 
 
 import turtle
+import random
 
 #aken
 aken = turtle.Screen()
 aken.setup(300,300)
 aken.title("Ülesanne 1")
 
-k1 = turtle.Turtle()
-k1.color('orange')
-k1.penup()
-k1.left(90)
-k1.pendown()
-k1.forward(100)
+varvid =('red','blue','orange','green')
+poora = 0
 
-k2 = turtle.Turtle()
-k2.color('red')
-k2.penup()
-k2.left(45)
-k2.pendown()
-k2.forward(100)
 
-k3 = turtle.Turtle()
-k3.color('light green')
-k3.penup()
-k3.right(0)
-k3.pendown()
-k3.forward(100)
+for i in range(8):
+    k1 = turtle.Turtle()
+    k1.speed(5)
+    k1.color(random.choice(varvid))
+    k1.left(poora)
+    k1.forward(100)
+    poora+=45
 
-k4 = turtle.Turtle()
-k4.color('orange')
-k4.penup()
-k4.right(45)
-k4.pendown()
-k4.forward(100)
-
-k5 = turtle.Turtle()
-k5.color('red')
-k5.penup()
-k5.right(90)
-k5.pendown()
-k5.forward(100)
-
-k6 = turtle.Turtle()
-k6.color('light green')
-k6.penup()
-k6.right(-45)
-k6.pendown()
-k6.forward(-100)
-
-k7 = turtle.Turtle()
-k7.color('blue')
-k7.penup()
-k7.right(0)
-k7.pendown()
-k7.forward(-100)
-
-k8 = turtle.Turtle()
-k8.color('blue')
-k8.penup()
-k8.left(-45)
-k8.pendown()
-k8.forward(-100)
+turtle.exitonclick()
