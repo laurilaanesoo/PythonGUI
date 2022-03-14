f1bc1966c1d06082495ca302f94df22760e4f0ad
laurilaanesoo <98745890+laurilaanesoo@users.noreply.github.com>
2022-03-14 09:22:58 +0200

    Ülesanne1

---
+++ Ülesanne1.py
@@ -0,0 +1,67 @@
+#Ülesanne 1
+#Lauri Laanesoo
+#14.03.2022
+
+
+import turtle
+
+#aken
+aken = turtle.Screen()
+aken.setup(300,300)
+aken.title("Ülesanne 1")
+
+k1 = turtle.Turtle()
+k1.color('orange')
+k1.penup()
+k1.left(90)
+k1.pendown()
+k1.forward(100)
+
+k2 = turtle.Turtle()
+k2.color('red')
+k2.penup()
+k2.left(45)
+k2.pendown()
+k2.forward(100)
+
+k3 = turtle.Turtle()
+k3.color('light green')
+k3.penup()
+k3.right(0)
+k3.pendown()
+k3.forward(100)
+
+k4 = turtle.Turtle()
+k4.color('orange')
+k4.penup()
+k4.right(45)
+k4.pendown()
+k4.forward(100)
+
+k5 = turtle.Turtle()
+k5.color('red')
+k5.penup()
+k5.right(90)
+k5.pendown()
+k5.forward(100)
+
+k6 = turtle.Turtle()
+k6.color('light green')
+k6.penup()
+k6.right(-45)
+k6.pendown()
+k6.forward(-100)
+
+k7 = turtle.Turtle()
+k7.color('blue')
+k7.penup()
+k7.right(0)
+k7.pendown()
+k7.forward(-100)
+
+k8 = turtle.Turtle()
+k8.color('blue')
+k8.penup()
+k8.left(-45)
+k8.pendown()
+k8.forward(-100)
