a3461e0f1b19b213584887287726e293c8aa3cfb
laurilaanesoo <98745890+laurilaanesoo@users.noreply.github.com>
2022-03-15 14:21:48 +0200

    Update Ülesanne4.py

--- Ülesanne4.py
+++ Ülesanne4.py
@@ -1,62 +1,71 @@
+#Ülesanne5
+#Lauri Laanesoo
+#15.03.2022
+
+
 from tkinter import *
 
-#akna seaded
+
 aken = Tk()
-aken.title('Tkinter "Hello World"')
-aken.iconbitmap('pythongui.png')
-aken.geometry("200x200")
+aken.title('Nupud')
 
-#Tekst
-tekst = Label(aken, text="Siia kunagi vastus!", font="Tahoma 12", pady=10, padx=30)
-tekst.grid(row=0, columnspan=4)
+aken.geometry("320x220")
 
+#funktsioonid
+def naita_valikut():
+    print(var.get())
+
+#valikukast
+var = IntVar()
+valikukast = Radiobutton(aken,text="9%", variable=var, value=9)
+valikukast.grid(row=1,column=1)
+valikukast = Radiobutton(aken,text="20%", variable=var, value=20)
+valikukast.grid(row=2,column=1)
+
+
+#sildid
+silt = Label(aken, text="Hind käibemaksuga:")
+silt.grid(row=0,column=0)
+
+silt2 = Label(aken, text="Käibemaksumäär")
+silt2.grid(row=1,column=0)
+
+joon = Label(aken, text="_______________________________________________________")
+joon.grid(row=6,columnspan=3)
+
+valjund = Label(aken, text="Käibemaks: ")
+valjund.grid(row=7,column=0)
+
+valjund2 = Label(aken, text="Hind käibemaksuga: ")
+valjund2.grid(row=8,columnspan=1)
+
+valjund4 = Label(aken, text="")
+valjund4.grid(row=7,column=1)
+
+valjund5= Label(aken, text="")
+valjund5.grid(row=8,column=1)
+
+
+#sisestusväljad
+sisestus = Entry(aken)
+sisestus.grid(row=0,column=1)
+
+
+#funktsioonid
+def arvutamine():
+    global kokku
+    global protsent
+    kokku = int(sisestus.get())
+    protsent = int(var.get())
+    kaibemaks = (kokku/100*protsent)
+    hind = kokku + kaibemaks
+    valjund4.config(text=f"{kaibemaks}€")
+    valjund5.config(text=f"{hind}€")
+    
+    
 #nupud
-nupp1 = Button(aken, text="7", width=5, padx=2)
-nupp1.grid(row=1, column=0)
+nupp1 = Button(aken, text="OK", width=10, command=arvutamine)
+nupp1.grid(row=16, column=2)
 
-nupp2 = Button(aken, text="8", width=5, padx=2)
-nupp2.grid(row=1, column=1)
-
-nupp3 = Button(aken, text="9", width=5, padx=2)
-nupp3.grid(row=1, column=2)
-
-nupp3 = Button(aken, text="/", width=5, padx=2)
-nupp3.grid(row=1, column=3)
-
-nupp4 = Button(aken, text="*", width=5, padx=2)
-nupp4.grid(row=2, column=3, pady=2)
-
-nupp5 = Button(aken, text="6", width=5, padx=2)
-nupp5.grid(row=2, column=2, pady=2)
-
-nupp6 = Button(aken, text="5", width=5, padx=2)
-nupp6.grid(row=2, column=1, pady=2)
-
-nupp7 = Button(aken, text="4", width=5, padx=2)
-nupp7.grid(row=2, column=0, pady=2)
-
-nupp8 = Button(aken, text="-", width=5, padx=2)
-nupp8.grid(row=3, column=3, pady=2)
-
-nupp9 = Button(aken, text="3", width=5, padx=2)
-nupp9.grid(row=3, column=2, pady=2)
-
-nupp10 = Button(aken, text="2", width=5, padx=2)
-nupp10.grid(row=3, column=1, pady=2)
-
-nupp11 = Button(aken, text="1", width=5, padx=2)
-nupp11.grid(row=3, column=0, pady=2)
-
-nupp12 = Button(aken, text="+", width=5, padx=2)
-nupp12.grid(row=4, column=3, pady=2)
-
-nupp13 = Button(aken, text="=", width=5, padx=2)
-nupp13.grid(row=4, column=2, pady=2)
-
-nupp14 = Button(aken, text=",", width=5, padx=2)
-nupp14.grid(row=4, column=1, pady=2)
-
-nupp15 = Button(aken, text="0", width=5, padx=2)
-nupp15.grid(row=4, column=0, pady=2)
-
+    
 aken.mainloop()
